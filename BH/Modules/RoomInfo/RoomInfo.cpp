b920c76e860291b3ed5622f033ec29025afb7087
hj <huang.jian@howtimeflies.io>
2019-11-26 10:06:02 +0800

    refactor RoomInfo with c++ lambda

--- BH/Modules/RoomInfo/RoomInfo.cpp
+++ BH/Modules/RoomInfo/RoomInfo.cpp
@@ -14,16 +14,42 @@ map<int, string> RoomInfo::mapAreaLevels;
 DWORD RoomInfo::gameTimer;
 string RoomInfo::txtDifficulty;
 
+vector<tuple<string, bool, function<string()>>> RoomInfo::toggleList;
+
 void RoomInfo::OnLoad() {
-    vector<string> toggleKeys = {
-        "Layer Level No Toggle",
-        "Area Level Toggle",
-        "Server Ip Toggle",
-        "Game Time Toggle",
-        "Clock Toggle",
+    RoomInfo::toggleList = {
+        make_tuple("Layer Level No Toggle", true, []() {
+            UnitAny* pUnit = D2CLIENT_GetPlayerUnit();
+            auto levelNo = pUnit->pPath->pRoom1->pRoom2->pLevel->dwLevelNo;
+            return wstring_to_string(D2CLIENT_GetLevelName(levelNo));
+        }),
+        make_tuple("Layer Level No Toggle", true, []() {
+            return RoomInfo::txtDifficulty;
+        }),
+        make_tuple("Area Level Toggle", true, []() {
+            UnitAny* pUnit = D2CLIENT_GetPlayerUnit();
+            auto levelNo = pUnit->pPath->pRoom1->pRoom2->pLevel->dwLevelNo;
+            int iLevelNo = levelNo & 0xffff; // map it to 1~65536
+            auto it = RoomInfo::mapAreaLevels.find(iLevelNo);
+            auto areaLevel = (it == mapAreaLevels.end()) ? "Unknown" : it->second;
+            return "Area level: " + areaLevel;
+        }),
+        make_tuple("Server Ip Toggle", true, []() {
+            return (*p_D2LAUNCH_BnData)->szGameIP;
+        }),
+        make_tuple("Clock Toggle", true, []() {
+            auto now = chrono::system_clock::to_time_t(chrono::system_clock::now());
+            return to_string(put_time(localtime(&now), "%X"));
+        }),
+        make_tuple("Game Time Toggle", true, []() {
+            auto now = GetTickCount();
+            int elapsedSeconds = (GetTickCount() - RoomInfo::gameTimer) / 1000;
+            return string_format("%.2d:%.2d:%.2d", elapsedSeconds / 3600, (elapsedSeconds / 60) % 60, elapsedSeconds % 60);
+        }),
     };
-    for (auto const& toggleKey : toggleKeys) {
-        BH::config->ReadToggle(toggleKey, "None", true, Toggles[toggleKey]);
+    for (auto const& item : toggleList) {
+        string key = get<0>(item);
+        BH::config->ReadToggle(key, "None", get<1>(item), Toggles[key]);
     }
 }
 
@@ -66,49 +92,16 @@ void RoomInfo::OnGameJoin() {
 }
 
 void RoomInfo::OnDraw() {
-    int yOffset = 8;
-    int xOffset = *p_D2CLIENT_ScreenSizeX - 16;
+    int yOffset = 6;
 
-    UnitAny* pUnit = D2CLIENT_GetPlayerUnit();
-    auto levelNo = pUnit->pPath->pRoom1->pRoom2->pLevel->dwLevelNo;
-
-    if (Toggles["Layer Level No Toggle"].state) {
-        auto levelName = D2CLIENT_GetLevelName(levelNo);
-        Texthook::Draw(xOffset, yOffset, Right, 0, Gold, levelName);
-        yOffset += 16;
-
-        Texthook::Draw(xOffset, yOffset, Right, 0, Gold, txtDifficulty);
-        yOffset += 16;
-    }
-
-    if (Toggles["Area Level Toggle"].state) {
-        int iLevelNo = levelNo & 0xffff; // map it to 1~65536
-        auto it = mapAreaLevels.find(iLevelNo);
-        auto areaLevel = (it == mapAreaLevels.end()) ? "Unknown" : it->second;
-        Texthook::Draw(xOffset, yOffset, Right, 0, Gold, "Area level: " + areaLevel);
-        yOffset += 16;
-    }
-
-    if (Toggles["Server Ip Toggle"].state) {
-        string serverIp = (*p_D2LAUNCH_BnData)->szGameIP;
-        if (!serverIp.empty()) {
-            Texthook::Draw(xOffset, yOffset, Right, 0, Gold, serverIp);
-            yOffset += 16;
+    for (auto const& item : toggleList) {
+        string key = get<0>(item);
+        if (Toggles[key].state) {
+            auto text = get<2>(item)();
+            if (!text.empty()) {
+                Texthook::Draw(*p_D2CLIENT_ScreenSizeX - 16, yOffset, Right, 0, Gold, text);
+                yOffset += 16;
+            }
         }
-    }
-
-    if (Toggles["Clock Toggle"].state) {
-        auto now = chrono::system_clock::to_time_t(chrono::system_clock::now());
-        string worldTime = to_string(put_time(localtime(&now), "%X"));
-        Texthook::Draw(xOffset, yOffset, Right, 0, Gold, worldTime);
-        yOffset += 16;
-    }
-
-    if (Toggles["Game Time Toggle"].state) {
-        auto now = GetTickCount();
-        int elapsedSeconds = (GetTickCount() - gameTimer) / 1000;
-        auto gameTime = string_format("%.2d:%.2d:%.2d", elapsedSeconds / 3600, (elapsedSeconds / 60) % 60, elapsedSeconds % 60);
-        Texthook::Draw(xOffset, yOffset, Right, 0, Gold, gameTime);
-        yOffset += 16;
     }
 }
--- BH/Modules/RoomInfo/RoomInfo.h
+++ BH/Modules/RoomInfo/RoomInfo.h
@@ -2,11 +2,14 @@
 
 #include "../../Config.h"
 #include "../Module.h"
+#include <tuple>
+#include <functional>
 
 class RoomInfo : public Module {
     private:
         // have to declare them as "static" as they are initialized in "OnGameJoin" 
         // but accessed in "OnAutomapDraw" (different threads???)
+        static vector<tuple<string, bool, function<string()>>> toggleList;
         static map<string, Toggle> Toggles;
         static map<int, string> mapAreaLevels;
         static DWORD gameTimer;
